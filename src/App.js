ccb3c35e293e337bf2784efbe4da7857a650fa96
Awadhesh Pal <awadheshpal@Awadheshs-MacBook-Pro.local>
2023-08-08 21:23:33 +0530

    delete and updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
  import './App.css';
  import TodoInput from './components/todoInput';
- import TodoList from './components/todolist';
+
 
 function App() {
   return (
      <div className='main-container'>
       <div className='centre-container'> 
       <TodoInput/>
-      <TodoList/>
       </div>
      </div>
   );
--- src/components/todoInput/index.js
+++ src/components/todoInput/index.js
@@ -3,34 +3,67 @@ import React, { useState } from "react";
 function TodoInput() {
   const [inputText, setInputText] = useState("");
   const [inputList, setInputList] = useState([]);
+  const [isEdit, setIsEdit] = useState(true)
 
   function handleClick() {
-    setInputList([
-      ...inputList,
-      { title: inputText },
-    ]);
-    setInputText('')
+    setInputList([...inputList, { title: inputText }]);
+    setInputText("");
+    setIsEdit(true)
   }
-  console.log(inputList)
+
+  function handleDeleteClick(title) {
+    const removeItem = inputList.filter((inputList) => {
+      return inputList.title !== title;
+    });
+    setInputList(removeItem)
+  }
+
+  function handleEditTodoChange(title){
+    setInputText(title)
+    setIsEdit(false)
+    // setInputList(
+    //   inputList.map((item)=>
+    //     title === item.title?{...inputList, title:inputText}: inputList
+    // )
+    // )
+
+    setInputList( inputList.map((item)=> { 
+      if(title === item.title){
+        return { ...item, title: inputText }
+      }
+      return item
+    }))
+
+    
+
+    
+    
+  }
+
   return (
     <div className="input-container">
       <input
         type="text"
         className="input-text"
         placeholder="Input your todo"
+        value={inputText}
         onChange={(e) => {
           setInputText(e.target.value);
           console.log(e.target.value);
         }}
       ></input>
-      <button className="add-button" onClick={handleClick} >
-        +
+      <button className="add-button" onClick={()=>{handleClick()}}>
+        {isEdit ? 'add' : "edit"}
       </button>
       {inputList?.map((inputList) => (
-        <div key={inputList.title}>
-            {inputList.title}
-        </div>
+        <div key={inputList.title}><p>{inputList.title}</p>
+        {inputList.title.length > 0 && <div key={inputList.title}> <button className="remove-list" onClick={()=>handleDeleteClick(inputList.title)}>
+        delete
+      </button>
+      <button className="edit-button" onClick={()=>handleEditTodoChange(inputList.title)}>edit</button> </div>}
+      </div>
       ))}
+     
     </div>
   );
 }
